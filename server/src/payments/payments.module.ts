4dfb98b6841a6fa38b48abc615a07467608529ce
Jeremy Cheung <efyjc12@nottingham.ac.uk>
2024-08-20 00:25:24 +0100

    Add TypeORM module imports

--- server/src/payments/payments.module.ts
+++ server/src/payments/payments.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { Payment } from './entities/payment.entity';
+import { Client } from '../clients/entities/client.entity';
+import { ClientsModule } from '../clients/clients.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Payment, Client]),  // Register Payment and Client entities
+    ClientsModule,  // Import ClientsModule to access ClientsService
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  exports: [PaymentsService],
 })
 export class PaymentsModule {}
